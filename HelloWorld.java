888acae1ffff6d4d6021a9c91acfc34a14dcb7d7
Dieter Halosta <dieter.halosta@yahoo.com>
2020-02-17 20:42:42 +0200

    Added a new word

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("Hello World! We are learning GIT");
+		System.out.println("Hello Daniela! We are learning GIT");
 	}
 }
